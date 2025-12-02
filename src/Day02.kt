97132b9be4cc3d5ae8ec3262dde9596a3705096c
bhaskar966 <deybhaskar999@gmail.com>
2025-12-02 13:12:41 +0530

    Day 2 solved

---
+++ src/Day02.kt
@@ -0,0 +1,72 @@
+
+fun main() {
+
+    fun part1(input: List<Pair<String, String>>): Long {
+
+        val ranges = input.map { it.first.toLong()..it.second.toLong() }
+        val invalidIds = mutableSetOf<Long>()
+
+        val maxDigits = ranges.maxOfOrNull { it.last.toString().length } ?: 0
+
+        var firstHalf = 1L
+        while (true) {
+            val s = firstHalf.toString()
+
+            if (s.length * 2 > maxDigits + 1 && maxDigits > 0) break
+
+            val repeatedS = s + s
+            val invalidId = repeatedS.toLongOrNull() ?: break
+
+            for(range in ranges) {
+                if(invalidId in range){
+                    invalidIds.add(invalidId)
+                }
+            }
+            firstHalf++
+        }
+
+        return invalidIds.sum()
+
+    }
+
+    fun part2(input: List<Pair<String, String>>): Long {
+        val ranges = input.map { it.first.toLong()..it.second.toLong() }
+        val invalidIds = mutableSetOf<Long>()
+
+        val maxDigits = ranges.maxOfOrNull { it.last.toString().length } ?: 0
+
+        var firstHalf = 1L
+        while (true) {
+            val s = firstHalf.toString()
+
+            if (s.length * 2 > maxDigits + 1 && maxDigits > 0) break
+
+            var repetitions = 2
+            while (true) {
+                val repeatedS = s.repeat(repetitions)
+
+                if(repeatedS.length > maxDigits && maxDigits > 0) break
+
+                val invalidId = repeatedS.toLongOrNull() ?: break
+
+                for(range in ranges) {
+                    if(invalidId in range){
+                        invalidIds.add(invalidId)
+                    }
+                }
+                repetitions++
+            }
+            firstHalf++
+        }
+
+        return invalidIds.sum()
+    }
+
+
+//    part1(readInputAndParsePair("Day02")).println()
+//    part1(readInputAndParsePair("day2test")).println()
+
+    part2(readInputAndParsePair("Day02")).println()
+//    part2(readInputAndParsePair("day2test")).println()
+
+}
--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,9 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+
+fun readInputAndParsePair(name: String) = Path("src/$name.txt").readText().split(',').map { str ->
+    val parts = str.split('-')
+    Pair(parts[0], parts[1])
+}
