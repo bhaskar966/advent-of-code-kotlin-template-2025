5f3d9d7ab74e4f78b6de3f712887be5c591c2c6d
bhaskar966 <deybhaskar999@gmail.com>
2025-12-07 10:53:16 +0530

    Remove unnecessary util functions for day 6

--- src/Utils.kt
+++ src/Utils.kt
@@ -167,31 +167,3 @@ fun List<String>.listifyWorkSheet(): List<List<Char>> {
     }
 
 }
-
-fun getOperandAndOperator(list: List<List<Char>>): List<Pair<List<Int>, Char>> {
-    val operands = mutableListOf<MutableList<Int>>()
-
-    for(line in 0..list.size-2){
-        operands.add(mutableListOf())
-        list[line].forEach { c ->
-            if(c.isDigit()) operands[line].add(c.digitToInt())
-        }
-    }
-
-    val operators = mutableListOf<Char>()
-    list.last().forEach {  c ->
-        operators.add(c)
-    }
-
-    return operators.mapIndexed { i, op ->
-        operands[i] to op
-    }
-}
-
-public inline fun <T> Iterable<T>.multiplicationOf(selector: (T) -> Int): Long {
-    var multiple: Long = 1L
-    for (element in this) {
-        multiple *= selector(element)
-    }
-    return multiple
-}
